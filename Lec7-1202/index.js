f1eb88c882de25fd2cea3261dcae69db29eee2c4
rishabhsharma8685 <sharmarishabh1162@gmail.com>
2025-02-12 15:43:45 +0530

    1202_JAVASCRIPT

---
+++ Lec7-1202/index.js
@@ -0,0 +1,51 @@
+// console.log(1);
+// console.log(sumOfTwo(10,5));
+
+// function sumOfTwo(a,b){
+//     return a+b;
+// }
+
+// setTimeout(()=>{
+//     console.log(4);
+// }, 2000);
+
+// var start=1;
+// var end = 1000000000000000;
+
+// while(start<end){
+//     start++;
+// }
+
+// console.log("end");
+
+// setInterval(()=>{
+//     console.log(9);
+// }, 4000);
+
+
+a();
+b();
+c();
+d();
+// function Statement
+function a(){
+    console.log("function statement");
+}
+
+// function expression
+
+var b = function(){
+    console.log("function expression");
+}
+
+// Named function expression
+
+var c = function xyz(){
+    console.log("Named function expression");
+}
+
+// arrow function 
+var d = () =>{
+    console.log("arrow function");
+}
+
