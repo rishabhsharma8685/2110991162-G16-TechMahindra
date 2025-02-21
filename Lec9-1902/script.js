d2e15c869590e2af1f0a9ab595d6a755db864e65
rishabhsharma8685 <sharmarishabh1162@gmail.com>
2025-02-21 11:41:51 +0530

    Completed pending work-Lec9-1902

--- Lec9-1902/script.js
+++ Lec9-1902/script.js
@@ -72,39 +72,39 @@
 
 // CALLBACK
 
-function reachedRestaurant(callback){
-    console.log("Reached Restaurant");
-    setTimeout(callback, 1000);
-}
+// function reachedRestaurant(callback){
+//     console.log("Reached Restaurant");
+//     setTimeout(callback, 1000);
+// }
 
-function TableFinding(callback){
-    console.log("Table Finding");
-    setTimeout(callback, 1000);
-}
+// function TableFinding(callback){
+//     console.log("Table Finding");
+//     setTimeout(callback, 1000);
+// }
 
-function MenuDiscover(callback){
-    console.log("Menu Discover");
-    setTimeout(callback, 1000);
-}
+// function MenuDiscover(callback){
+//     console.log("Menu Discover");
+//     setTimeout(callback, 1000);
+// }
 
-function starterOrder(callback){
-    console.log("Starter Order");
-    setTimeout(callback, 1000);
-}
+// function starterOrder(callback){
+//     console.log("Starter Order");
+//     setTimeout(callback, 1000);
+// }
 
-function mainCourse(callback){
-    console.log("Main Course Order");
-    setTimeout(callback, 1000);
-}
+// function mainCourse(callback){
+//     console.log("Main Course Order");
+//     setTimeout(callback, 1000);
+// }
 
-function dessert(callback){
-    console.log("Dessert Order");
-    setTimeout(callback, 1000);
-}
+// function dessert(callback){
+//     console.log("Dessert Order");
+//     setTimeout(callback, 1000);
+// }
 
-    function payBill(){
-    console.log("PayBill");
-}
+//     function payBill(){
+//     console.log("PayBill");
+// }
 
 // Chaining the Callbacks
 
@@ -139,7 +139,7 @@ function dessert(callback){
 //     else{
 //         reject("can't reach on time");
 //     }
-// }  pending ,,,, do it from gh
+// }  
 
 
 // const githubData = fetch("https://api.github.com/rishabhsharma8685");
